6c933ed354ed912346fbb5ebd3bc9f4eae32a6f4
Robbi Garza <xaleth09@gmail.com>
2024-05-22 17:34:06 -0700

    refactor: restyle Card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import {Column} from "./Flex";
 import styled from "styled-components";
 
-type Props = {
-    children: React.ReactNode
-}
-
 const StyledColumn = styled(Column)`
     background-color: white;
     border-radius: 4px;
-    border: solid grey 1px;
+    border: solid lightgray 1px;
     padding: 16px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
 `
+
+type Props = {
+    children: React.ReactNode
+}
 
 export const Card = ({children}: Props) => {
     return (
