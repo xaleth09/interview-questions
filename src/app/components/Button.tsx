19754b89d5212a72745cf3269d5302a96781ee76
Robbi Garza <xaleth09@gmail.com>
2024-05-22 17:33:54 -0700

    feat: add Button to component library

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from "styled-components";
+
+type StyledButtonProps = {
+    type?: "primary" | "secondary" | "tertiary";
+    size?: 'inline' | 'fullwidth';
+    disabled?: boolean;
+    loading?: boolean;
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
+    ${({size, disabled, loading}) => `
+        width: ${size === 'fullwidth' ? '100%' : 'initial'};
+        color: 
+    `}
+`
+
+type Props = {
+    onClick?: (event?: React.MouseEvent<HTMLButtonElement>) => void;
+    children: string;
+} & StyledButtonProps;
+
+export const Button = ({
+                           size = 'fullwidth',
+                           disabled,
+                           loading,
+                           onClick,
+                           children,
+                       }: Props) => {
+
+    const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        if (onClick) {
+            onClick(event);
+        }
+    }
+
+    return (
+        <StyledButton
+            size={size}
+            disabled={disabled}
+            loading={loading}
+            onClick={handleOnClick}
+        >
+            {children}
+        </StyledButton>
+    )
+}
